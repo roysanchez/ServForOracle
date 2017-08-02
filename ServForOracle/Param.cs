704edc67201d15ccc14825b9625f971c6c7cda17
Roy Sanchez <roysanchez@outlook.com>
2017-08-02 11:16:51 -0400

    Updated the param class to handle the OracleDbType as an optional parameter, to solve the case when the library can't correctly predict the OracleDbType that is used by the package/proc/function.

--- ServForOracle/Param.cs
+++ ServForOracle/Param.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
 
 namespace ServForOracle
 {
     public class Param
     {
-        private Param(ParamDirection direction, Type type, object value)
+        private Param(ParamDirection direction, Type type, object value, OracleDbType? oracleType = null)
         {
             if (typeof(ParamDirection) == type || typeof(Param) == type)
                 throw new ArgumentException($"Can't use {type.Name} as a parameter type", nameof(type));
@@ -17,19 +18,22 @@ namespace ServForOracle
             ParamDirection = direction;
             Value = value;
             Type = type;
+            OracleType = oracleType;
         }
 
         public ParamDirection ParamDirection { get; set; }
         public object Value { get; set; }
         public Type Type { get; set; }
 
-        public static Param Create<T>(ParamDirection direction, T value)
+        internal OracleDbType? OracleType { get; set; }
+
+        public static Param Create<T>(ParamDirection direction, T value, OracleDbType? oracleType = null)
         {
             var type = typeof(T);
             if (!ParamHandler.IsValidParameterType(type))
                 throw new ArgumentException(string.Format(ParamHandler.InvalidClassMessage, type.Name));
 
-            return new Param(direction, type, value);
+            return new Param(direction, type, value, oracleType);
         }
 
         public static Param Input<T>(T value = default(T)) => Create(ParamDirection.Input, value);
