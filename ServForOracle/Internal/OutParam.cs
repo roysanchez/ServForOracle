774e8f2f70b3c062cdc7e4d8f6cee282ea74ef68
Roy Sanchez <roysanchez@outlook.com>
2018-09-04 09:07:42 -0400

    Added the missing documentation to the OutParam class

--- ServForOracle/Internal/OutParam.cs
+++ ServForOracle/Internal/OutParam.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 
 namespace ServForOracle.Internal
 {
+    /// <summary>
+    /// Type casted implementation of the <see cref="OutParam"/> base class
+    /// </summary>
+    /// <typeparam name="T">The expected <see cref="Type"/> to be returned by the <see cref="OracleParameter"/></typeparam>
     internal class OutParam<T>: OutParam
     {
         public OutParam(Param<T> param, OracleParameter oracleParameter)
@@ -18,8 +22,16 @@ namespace ServForOracle.Internal
             OutParameter = oracleParameter;
         }
 
+        /// <summary>
+        /// The parameter definition set by the user
+        /// </summary>
         public new Param<T> ServiceParameter { get; set; }
 
+        /// <summary>
+        /// Sets the value of the <see cref="ServiceParameter"/> by calling 
+        /// <see cref="ParamHandler.ConvertOracleParameterToBaseType{T}(OracleParameter)"/>
+        /// </summary>
+        /// <exception cref="Exception">catches and gives more context to any exception generated in the convertion process</exception>
         public override void SetParamValue()
         {
             try
@@ -37,6 +49,11 @@ namespace ServForOracle.Internal
         }
     }
 
+    /// <summary>
+    /// The class is used to segregate the <see cref="ParamDirection.Output"/>/<see cref="ParamDirection.InputOutput"/>
+    /// <see cref="OracleParameter"/> objects from the <see cref="ParamDirection.Input"/> for later processing, as they
+    /// required to be casted to the expected <see cref="Type"/>.
+    /// </summary>
     internal abstract class OutParam
     {
         public OutParam(Param param, OracleParameter oracleParameter)
@@ -45,9 +62,18 @@ namespace ServForOracle.Internal
             OutParameter = oracleParameter;
         }
 
+        /// <summary>
+        /// The parameter definition send by the user
+        /// </summary>
         public Param ServiceParameter { get; set; }
+        /// <summary>
+        /// The <see cref="OracleParameter"/> recieved by the library after executing the command
+        /// </summary>
         public OracleParameter OutParameter { get; set; }
 
+        /// <summary>
+        /// Sets the value of the <see cref="OutParam.ServiceParameter"/> from the <see cref="OutParam.OutParameter"/>
+        /// </summary>
         public abstract void SetParamValue();
     }
 }
