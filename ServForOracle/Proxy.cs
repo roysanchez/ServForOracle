6c7b77443303acaec5238057c44d0abb35152135
Roy Sanchez <roysanchez@outlook.com>
2018-09-04 10:08:35 -0400

    Added the comments to the Proxy Utility class

--- ServForOracle/Proxy.cs
+++ ServForOracle/Proxy.cs
@@ -7,13 +7,36 @@ using System.Threading.Tasks;
 
 namespace ServForOracle
 {
+    /// <summary>
+    /// Used to create proxies for either Oracle UDT Collections or Objects that don't have the 
+    /// <see cref="UDTCollectionNameAttribute"/> or <see cref="UDTNameAttribute"/>
+    /// </summary>
     public static class Proxy
     {
+        /// <summary>
+        /// Creates a collection proxy for the <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The underlying type to use for the collection</typeparam>
+        /// <param name="udtCollectionName">The name of the Oracle UDT Collection</param>
+        /// <exception cref="Exception">If the <typeparamref name="T"/> is not a CLR or a class with 
+        /// the <see cref="UDTNameAttribute"/> it will raise an exception
+        /// </exception>
+        /// <remarks>Wrapper over <see cref="CreateListType(Type, string)"/></remarks>
+        /// <seealso cref="CreateListType(Type, string)"/>
         public static void CreateListType<T>(string udtCollectionName)
         {
             CreateListType(typeof(T), udtCollectionName);
         }
 
+        /// <summary>
+        /// Creates a collection proxy for the <paramref name="underlyingType"/>
+        /// </summary>
+        /// <param name="underlyingType">The underlying type to use for the collection</param>
+        /// <param name="udtCollectionName">The name of the Oracle UDT Collection</param>
+        /// <seealso cref="CreateListType{T}(string)"/>
+        /// <exception cref="ArgumentNullException">if either <paramref name="underlyingType"/> or 
+        /// <paramref name="udtCollectionName"/> is null or empty
+        /// </exception>
         public static void CreateListType(Type underlyingType, string udtCollectionName)
         {
             if(underlyingType == null)
@@ -28,11 +51,27 @@ namespace ServForOracle
             ProxyFactory.GetOrCreateProxyCollectionType(underlyingType, udtCollectionName);
         }
 
+        /// <summary>
+        /// Creates an object proxy for the <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The base type for the proxy</typeparam>
+        /// <param name="udtName">The name of the Oracle UDT object</param>
+        /// <remarks>Wrapper over <see cref="CreateType(Type, string)"/></remarks>
+        /// <seealso cref="CreateType(Type, string)"/>
         public static void CreateType<T>(string udtName)
         {
             CreateType(typeof(T), udtName);
         }
 
+        /// <summary>
+        /// Creates an object proxy for the <paramref name="type"/>
+        /// </summary>
+        /// <param name="type">The base type for the proxy</param>
+        /// <param name="udtName">The name of the Oracle UDT object</param>
+        /// <seealso cref="CreateType{T}(string)"/>
+        /// <exception cref="ArgumentNullException">If either the <paramref name="type"/> or <paramref name="udtName"/> 
+        /// is null or empty.
+        /// </exception>
         public static void CreateType(Type type, string udtName)
         {
             if(type == null)
