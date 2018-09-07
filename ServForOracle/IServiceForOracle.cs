4128ccb8b91808585649f8baf3b1d14670f2421b
Roy Sanchez <roysanchez@outlook.com>
2018-09-07 09:16:45 -0400

    Added missing synchronous methods to the IServiceForOracle interface.

--- ServForOracle/IServiceForOracle.cs
+++ ServForOracle/IServiceForOracle.cs
@@ -12,6 +12,12 @@ namespace ServForOracle
         /// Executes the DDL specified
         /// </summary>
         /// <param name="ddl">The DDL to execute</param>
+        void ExecuteDDL(string ddl);
+
+        /// <summary>
+        /// Executes the DDL specified
+        /// </summary>
+        /// <param name="ddl">The DDL to execute</param>
         /// <returns>A task indicating he result of the execution</returns>
         Task ExecuteDDLAsync(string ddl);
 
@@ -22,8 +28,26 @@ namespace ServForOracle
         /// <param name="function">The function to execute, which follows this format: schema.function or schema.package.function</param>
         /// <param name="parameters">The parameters of the function  whether IN, OUT or IN OUT</param>
         /// <remarks>The OUT and IN OUT parameters get their values set by this method</remarks>
+        /// <returns>The result of the execution and containing the return value if it is successful</returns>
+        T ExecuteFunction<T>(string function, params Param[] parameters);
+
+        /// <summary>
+        /// Executes an Oracle Function
+        /// </summary>
+        /// <typeparam name="T">The espected result of the oracle function</typeparam>
+        /// <param name="function">The function to execute, which follows this format: schema.function or schema.package.function</param>
+        /// <param name="parameters">The parameters of the function  whether IN, OUT or IN OUT</param>
+        /// <remarks>The OUT and IN OUT parameters get their values set by this method</remarks>
         /// <returns>A task indicating the result of the execution and containing the return value if it is successful</returns>
         Task<T> ExecuteFunctionAsync<T>(string function, params Param[] parameters);
+
+        /// <summary>
+        /// Executes an Oracle procedure
+        /// </summary>
+        /// <param name="procedure">The procedure to run, with the format schema.procedure or schema.package.procedure</param>
+        /// <param name="parameters">The parameters for the procedure, whether IN, OUT or IN OUT</param>
+        /// <remarks>The OUT and IN OUT parameters get their values set by this method</remarks>
+        void ExecuteProcedure(string procedure, params Param[] parameters);
 
         /// <summary>
         /// Executes an Oracle procedure
